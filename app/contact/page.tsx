b9e9a878181edd1d74df34af5f40878dbb516372
Mbareck21 <missara.lamine@gmail.com>
2024-07-24 22:45:25 -0500

    adding message slider

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from "react";
 import { z } from "zod";
+
 const contactFormSchema = z.object({
   name: z.string().min(1, "Name is required"),
   email: z.string().email("Invalid email address"),
