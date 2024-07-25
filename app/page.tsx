439898bf94b63f014d802d6fdf98eaa123adb4ce
Mbareck21 <missara.lamine@gmail.com>
2024-07-24 21:03:38 -0500

    adding message slider

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
 import Hero from "@/components/Hero";
+import SlidingMessages from "@/components/SlidingMessages";
 
 export default function Home() {
-  return <Hero />;
+  return (
+    <div className="container space-y-10">
+      <section className="relative">
+        <Hero />
+      </section>
+      <section className="relative">
+        <SlidingMessages />
+      </section>
+    </div>
+  );
 }
---
+++ components/SlidingMessages.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function SlidingMessages() {
+  return (
+    <div className="rounded-2xl h-[10rem] w-80 mx-auto p-4 overflow-hidden bg-stone-700 border border-transparent dark:border-white/[0.2] group-hover:border-slate-700 sticky bottom-5 ">
+      SlidingMessages
+    </div>
+  );
+}
