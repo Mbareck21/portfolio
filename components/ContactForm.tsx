ee00a7f6649a2f4af72d30e6a11408bdfe40b1d3
Mbareck21 <missara.lamine@gmail.com>
2024-07-31 16:56:35 -0500

    adding message slider

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -3,7 +3,6 @@ import Link from "next/link";
 import { useState } from "react";
 import { z } from "zod";
 
-
 const contactFormSchema = z.object({
   name: z.string().min(3, { message: "Must be 3 or more characters long" }),
   email: z.string().email("Invalid email address"),
