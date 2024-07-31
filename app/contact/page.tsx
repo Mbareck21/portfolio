21839079597fedd72446c1fee02ddd360dcda39f
Mbareck21 <missara.lamine@gmail.com>
2024-07-31 15:29:21 -0500

    adding message slider

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -11,9 +11,9 @@ export default function Form() {
         </h1>
         <p className="mt-8 text-zinc-400 tracking-wide leading-relaxed p-4 lg: w-[60%] md:w-full sm: w-full">
           This component allows for basic form submission and, and it sends
-          visitor's name, email and message to{" "}
-          <span className="text-sky-500">Mongodb</span>. I'd then fetch the same
-          data and display it on the home page. I used{" "}
+          visitor&apos;s name, email and message to{" "}
+          <span className="text-sky-500">Mongodb</span>. I&apos;d then fetch the
+          same data and display it on the home page. I used{" "}
           <span className="text-sky-500">Next.Js server</span> to handle the
           submission and <span className="text-sky-500">Mongoose</span> to build
           the data <span className="text-sky-500">schema</span> and handle the
